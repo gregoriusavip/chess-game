12917889691af20233c824ecfcac97d10a3f7fe1
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2025-01-09 20:57:22 -0800

    Add method to check a square
    Board size is changed to 10x12 1D to handle pieces' movements.

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,5 +1,6 @@
 # frozen-string-literal: true
 
+require_relative('pieces/piece')
 require_relative('pieces/pawn')
 require_relative('pieces/rook')
 require_relative('pieces/knight')
@@ -12,13 +13,52 @@ class ChessBoard
   attr_accessor :white_pieces, :black_pieces
 
   def initialize
-    @board = Array.new(64, 0) # one dimensional 8x8 board
+    @board = Array.new(120, 0) # one dimensional 10x12 board
+    # TODO: Assign squares outside of 8x8 playing field as nil
+  end
+
+  # given a color and position, return true or false if the square is in check by the opposite color
+  def check_square(color, pos)
+    yield_pieces(pos, [9, -9, 11, -11]) { |piece| opponent_piece?(piece, color, [King, Queen, Bishop]) } ||
+      yield_pieces(pos, [10, -10, -1, 1]) { |piece| opponent_piece?(piece, color, [King, Queen, Rook]) } ||
+      opponent_knight?(pos, color) ||
+      opponent_pawn?(pos, color)
   end
 
   private
 
+  attr_accessor :board
+
   def init_board
     # TODO: put all of the pieces at the start of the round
+  end
+
+  def opponent_piece?(piece, color, piece_types)
+    true if piece_types.include?(piece.class) && piece.color != color
+  end
+
+  def yield_pieces(pos, moves)
+    moves.each do |move|
+      cur_pos = pos + move
+      cur_pos += move until @board[cur_pos].nil? || @board[cur_pos].is_a?(Piece)
+      yield @board[cur_pos] unless @board[cur_pos].nil?
+    end
+  end
+
+  def opponent_knight?(pos, color)
+    moves = [8, 19, 21, 12, -8, -19, -21, -12]
+    moves.each do |move|
+      cur_pos = pos + move
+      return true if opponent_piece?(@board[cur_pos], color, [Knight])
+    end
+  end
+
+  def opponent_pawn?(pos, color)
+    moves = (color == :black ? [-9, -11] : [9, 11])
+    moves.each do |move|
+      cur_pos = pos + move
+      return true if opponent_piece?(@board[cur_pos], color, [Pawn])
+    end
   end
 
   def debug_print

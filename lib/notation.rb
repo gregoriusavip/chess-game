7b0d3ab64306fc43ef2996231062b8cb7f343b8e
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2025-01-22 20:33:19 -0800

    Add module to create/convert to chess notation

---
+++ lib/notation.rb
@@ -0,0 +1,9 @@
+# frozen-string-literal: true
+
+# handles converting from chess notation to board position and vice versa
+module Notation
+  def to_notation(pos)
+    files = { 1 => 'a', 2 => 'b', 3 => 'c', 4 => 'd', 5 => 'e', 6 => 'f', 7 => 'g', 8 => 'h' }
+    [files[(pos % 10)], ((pos - 10) / 10)]
+  end
+end

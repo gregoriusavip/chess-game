d92ea0b7a9d84e9cb57a0f1e23e63bdc67cfec6c
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2025-01-25 21:16:56 -0800

    Switch notation to white's perspective

--- lib/notation.rb
+++ lib/notation.rb
@@ -4,6 +4,6 @@
 module Notation
   def to_notation(pos)
     files = { 1 => 'a', 2 => 'b', 3 => 'c', 4 => 'd', 5 => 'e', 6 => 'f', 7 => 'g', 8 => 'h' }
-    [files[(pos % 10)], ((pos - 10) / 10)]
+    [files[(pos % 10)], ((110 - pos)) / 10]
   end
 end

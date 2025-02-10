522a37b59e6e1e1dfca0cc439d27db87b48c2418
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2025-02-09 21:12:11 -0800

    Add functions to handle some moves
    DONE: en passant and standard moves
    TODO: king castles

--- lib/board/controller.rb
+++ lib/board/controller.rb
@@ -10,24 +10,76 @@ module ChessController
     moves = color == :white ? white_moves : black_moves
     loop do
       pos = piece_input(color)
-      target = pos_input
-      return [pos, target] if moves.include?(pos) && moves[pos].include?(target)
+      t_not = pos_input
+      return [pos, t_not, moves[pos][t_not]] if moves.include?(pos) && moves[pos].include?(t_not)
 
       puts 'this move is not legal'
     end
   end
 
-  def make_move(piece, t_notation)
-    @board[piece.pos] = 0
-    target = to_position(t_notation)
-    target_piece = @board[target]
-    @board[target] = piece
-    piece.pos = target
-    piece.moved = true
-    remove_piece(target_piece)
+  def move(piece, t_not, move_type)
+    piece.color == :black ? (@double_push_black = nil) : (@double_push_white = nil)
+    current_pos = piece.pos
+    target = to_position(t_not)
+
+    # TODO: ADD OTHER MOVE FUNCTION
+    case move_type
+    when :en_passant
+      en_passant_move(piece, target)
+    else
+      standard_move(piece, target)
+    end
+    return unless piece.instance_of?(Pawn)
+
+    pawn_moves(piece, current_pos, target)
   end
 
   private
+
+  # --- Functions for each move types
+
+  def standard_move(piece, target)
+    move_helper(piece, target)
+    @board[target] = piece
+    piece.pos = target
+  end
+
+  def en_passant_move(piece, target)
+    offset = piece.color == :black ? 10 : -10
+    move_helper(piece, target)
+    @board[target] = 0
+    @board[target + offset] = piece
+    piece.pos = target + offset
+  end
+  # TODO: ADD OTHER MOVE FUNCTION
+
+  # helper function for most move type
+  def move_helper(piece, target)
+    @board[piece.pos] = 0
+    target_piece = @board[target]
+    piece.moved = true unless piece.moved
+    remove_piece(target_piece)
+  end
+
+  def pawn_moves(piece, pos, target)
+    return if promo(piece, target)
+
+    double_push(piece, pos, target)
+  end
+
+  def double_push(piece, pos, target)
+    return unless (target - pos).abs == 20
+
+    piece.color == :black ? (@double_push_black = target) : (@double_push_white = target)
+  end
+
+  # TODO: HANDLE PROMO FUNCTION
+  def promo(piece, target)
+    return false unless [1, 8].include?((target - 110) / 10)
+
+    print 'promo'
+    true
+  end
 
   def piece_input(color)
     loop do
